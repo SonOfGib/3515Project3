c388f127a8e11223ab87183e1086b18f99e4de5c
Gean1918 <seanmoner@gmail.com>
2018-02-13 21:16:45 -0500

    Fixing cod

--- app/src/main/java/edu/temple/a3515project3/PaletteActivity.java
+++ app/src/main/java/edu/temple/a3515project3/PaletteActivity.java
@@ -2,6 +2,8 @@ package edu.temple.a3515project3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class PaletteActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class PaletteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palette);
+
+        //colors
+        String colors[] = {"White","Red", "Blue", "Green", "Yellow", "Purple"};
+
+        Spinner paletteSpinner = findViewById(R.id.paletteSpinner);
+        paletteSpinner.setAdapter(new PaletteAdapter(colors));
     }
 }
--- app/src/main/java/edu/temple/a3515project3/PaletteAdapter.java
+++ app/src/main/java/edu/temple/a3515project3/PaletteAdapter.java
@@ -1,8 +1,46 @@
 package edu.temple.a3515project3;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
 /**
- * Created by seanmoner on 2/13/2018.
+ * Custom adapter that changes the TextView background to the color of the text it contains.
+ * Created by Sean Gibson on 2/13/2018.
  */
 
-public class PaletteAdapter {
+public class PaletteAdapter extends BaseAdapter{
+    String[] colors;
+    public PaletteAdapter(String[] colors) {
+        this.colors = colors;
+    }
+
+    @Override
+    public int getCount() {
+        return colors.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return colors[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        TextView textView = (TextView) view;
+        if(view == null){
+            view = new TextView();
+        }
+        textView.setText(colors[i]);
+        textView.setBackgroundColor(Color.parseColor(colors[i]));
+        return view;
+    }
 }
